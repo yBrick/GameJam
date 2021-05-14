9dd5391c606de6b7bd1cc26efbcf629435a3b6c8
Joe-Stanek <84087946+Joe-Stanek@users.noreply.github.com>
2021-05-14 10:18:52 -0500

    Update Blackjack.java

--- Blackjack.java
+++ Blackjack.java
@@ -1,4 +1,18 @@
+
 public class Blackjack
 {
+  Dealer d = new Dealer();
   
+  public void startGame()
+  {
+    d.Deal();
+
+    System.out.println("Cards Equal: " + d.cardValue;
+    
+    
+    
+  }
+      
+      
+      
 }
