f77c24b3e228743d349c062db134bc5a599ca0e8
Joe-Stanek <84087946+Joe-Stanek@users.noreply.github.com>
2021-05-14 14:56:49 -0500

    Update Blackjack.java

--- Blackjack.java
+++ Blackjack.java
@@ -1,6 +1,10 @@
+import java.util.Scanner();
+
 
 public class Blackjack
 {
+  Scanner sc = new Scanner(System.in);
+  
   Dealer d = new Dealer();
   
   public void startGame()
@@ -9,10 +13,43 @@ public class Blackjack
 
     System.out.println("Cards Equal: " + d.cardValue;
     
-    
+    Choice();
     
   }
       
+                       
+   public int Choice()
+   {
+     System.out.println("Please select an option:\n 1. Hit \n 2. Stay \n 3. Bet");
+     
+     int gameChoice = sc.nextInt();
+     
+     if(gameChoice = 1)
+     {
+       d.Hit();
+     }
+     else if(gameChoice = 2)
+     {
+       d.Stay();
+     }
+     else if(gameChoice = 3)
+     {
+       d.AddBet();
+     }
+     else()
+     {
+       Sytsem.out.println("Please choose a valid option");
+       Choice();
+     }
+     
+     
+     public void continueGame()
+     {
+       
+     }
+     
+   }
+                        
       
       
 }
