84ce6f51a788a4218df9f44dc596e2976756962a
Joe-Stanek <84087946+Joe-Stanek@users.noreply.github.com>
2021-05-14 21:50:17 -0500

    Update Dealer.java

--- Dealer.java
+++ Dealer.java
@@ -1,8 +1,10 @@
 public class Dealer
 {
+  int cardAmt = 2;
   public void Hit()
   {
-    
+    cardAmt = cardAmt +1; //need to add more, still thinking of an idea
+   
   }
   
   public void Stay()
