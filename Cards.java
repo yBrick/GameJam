1fe6129e17216b5b37dd3708febabc179c105fb5
ZachHerr26 <84141109+ZachHerr26@users.noreply.github.com>
2021-05-18 10:28:10 -0500

    Update Cards.java

--- Cards.java
+++ Cards.java
@@ -2,25 +2,42 @@ import java.util.ArrayList;
 import java.util.Array;
 public class Cards
 {
-  public rules()
-  {
-    System.out.println("");
-  }
+    String[] cardRowChar = new String[]{"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+    String[] intArray = new String[]{"--------------","|            |","|            |","void","|            |","|            |","______________",};
+    public void rules()
+    {
+        System.out.println("");
+    }
+
+    public void test()
+    {
+     ArrayList<int> str = new ArrayList<int>();
+       str.add(0);
+       str.add(9);
+       str.add(12); 
+      printCards(str);
+    }
   
-  public printCards(ArrayList <int> cardList)
-  {
-   for(int i = 0; i<7; i++)
-   {
-     for(int j = 0; j < cardList.size(); j++)
-     {
-       if(i==3)
-       {
-         System.out.print("|        " + cardRowChar + "       |");
-       }
-       else
-       {System.out.print(cardRow[i]);}
-     }
-   }
- }
+    public void printCards(ArrayList <int> cardList)
+    {
+        for(int i = 0; i<7; i++)
+        {
+            for(int j = 0; j < cardList.size(); j++)
+            {
+                if(i == 3 && cardRowChar[cardList[j]].equals("10"))
+                {
+                    {
+                        System.out.print("|     10     |");
+                    }
+                }
+                else if(i==3)
+                {
+                    System.out.print("|      " + cardRowChar[cardList.get(j)] + "     |");
+                }
+                else
+                {System.out.print(cardRow[i]);}
+            }
+        }
+    }
 
 }
