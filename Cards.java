93edfabaa255c5ff5bb48c9502b29ef8e7b43320
yBrick <74314017+yBrick@users.noreply.github.com>
2021-05-18 09:56:01 -0500

    Update Cards.java

--- Cards.java
+++ Cards.java
@@ -6,65 +6,85 @@ public class Cards
   }
 
 
-  public String getCard(int card)
-  {
-    if(card == 0)
+ public String getCard(int card)
     {
-      String 1 = "---------------\n|                |";
-                  
-                 
-    }
-    else if(card == 1)
-    {
+        if(card == 0)
+        {
+            String x = " ________________\n|                |\n|                |\n|        A       |\n|                |\n|                |\n------------------";
+            return x;
+        }
+        else if(card == 1)
+        {
+            String 1 = " _____________\n|             |\n|             |\n|             |\n|      1      |\n|             |\n|             |\n|             |\n---------------";
+            return x;
+        }
+        else if(card == 2)
+        {
+            String x = " _____________\n|             |\n|             |\n|             |\n|      2      |\n|             |\n|             |\n|             |\n---------------";
+            return x;
+        }
+        else if(card == 3)
+        {
+            String x = " _____________\n|             |\n|             |\n|             |\n|      3      |\n|             |\n|             |\n|             |\n---------------";
+            return x;
+        }
+        else if(card == 4)
+        {
+            String x = " _____________\n|             |\n|             |\n|             |\n|      4      |\n|             |\n|             |\n|             |\n---------------";
+            return x;
+        }
+        else if(card == 5)
+        {
+            String x = " _____________\n|             |\n|             |\n|             |\n|      5      |\n|             |\n|             |\n|             |\n---------------";
+            return x;
+        }
+        else if(card == 6)
+        {
+            String x = " _____________\n|             |\n|             |\n|             |\n|      6      |\n|             |\n|             |\n|             |\n---------------";
+            return x;
+        }
+        else if(card == 7)
+        {
+            String x = " _____________\n|             |\n|             |\n|             |\n|      7      |\n|             |\n|             |\n|             |\n---------------";
+            return x;
+        }
+        else if(card == 8)
+        {
+            String x = " _____________\n|             |\n|             |\n|             |\n|      8      |\n|             |\n|             |\n|             |\n---------------";
+            return x;
+        }
+        else if(card == 9)
+        {
+            String x = " _____________\n|             |\n|             |\n|             |\n|      9      |\n|             |\n|             |\n|             |\n---------------";
+            return x;
+        }
+        else if(card == 10)
+        {
+            String x = " _____________\n|             |\n|             |\n|             |\n|      10     |\n|             |\n|             |\n|             |\n---------------";
+            return x;
+        }
+        else if(card == 11)
+        {
+            String x = " _____________\n|             |\n|             |\n|             |\n|      J      |\n|             |\n|             |\n|             |\n---------------";
+            return x;
+        }
+        else if(card == 12)
+        {
+            String x = " _____________\n|             |\n|             |\n|             |\n|      Q      |\n|             |\n|             |\n|             |\n---------------";
+            return x;
+        }
+        else if(card == 13)
+        {
+            String x = " _____________\n|             |\n|             |\n|             |\n|      K      |\n|             |\n|             |\n|             |\n---------------";
+            return x;
+        }
+        else
+        {
+            String e = "error";
+            return e;
+        }
+
       
-    }
-    else if(card == 2)
-    {
-      
-    }
-    else if(card == 3)
-    {
-      
-    }
-    else if(card == 4)
-    {
-      
-    }
-    else if(card == 5)
-    {
-      
-    }
-    else if(card == 6)
-    {
-      
-    }
-    else if(card == 7)
-    {
-      
-    }
-    else if(card == 8)
-    {
-      
-    }
-    else if(card == 9)
-    {
-      
-    }
-    else if(card == 10)
-    {
-      
-    }
-    else if(card == 11)
-    {
-      
-    }
-    else if(card == 12)
-    {
-      
-    }
-    else if(card == 13)
-    {
-      String 
     }
     
   }
