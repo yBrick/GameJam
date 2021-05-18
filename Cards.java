ca3a43b88e190a009a4e12fe0cd0c54da300ddf5
ZachHerr26 <84141109+ZachHerr26@users.noreply.github.com>
2021-05-18 10:08:24 -0500

    Create Cards.java

--- Cards.java
+++ Cards.java
@@ -1,91 +1,26 @@
+import java.util.ArrayList;
+import java.util.Array;
 public class Cards
 {
   public rules()
   {
-  System.out.println("");
+    System.out.println("");
   }
+  
+  public printCards(ArrayList <int> cardList)
+  {
+   for(int i = 0; i<7; i++)
+   {
+     for(int j = 0; j < cardList.size(); j++)
+     {
+       if(i==3)
+       {
+         System.out.print("|        " + cardRowChar + "       |");
+       }
+       else
+       {System.out.print(cardRow[i]);}
+     }
+   }
+ }
 
-
- public String getCard(int card)
-    {
-        if(card == 0)
-        {
-            String x = " ________________\n|                |\n|                |\n|        A       |\n|                |\n|                |\n------------------";
-            return x;
-        }
-        else if(card == 1)
-        {
-            String 1 = " _____________\n|             |\n|             |\n|             |\n|      1      |\n|             |\n|             |\n|             |\n---------------";
-            return x;
-        }
-        else if(card == 2)
-        {
-            String x = " _____________\n|             |\n|             |\n|             |\n|      2      |\n|             |\n|             |\n|             |\n---------------";
-            return x;
-        }
-        else if(card == 3)
-        {
-            String x = " _____________\n|             |\n|             |\n|             |\n|      3      |\n|             |\n|             |\n|             |\n---------------";
-            return x;
-        }
-        else if(card == 4)
-        {
-            String x = " _____________\n|             |\n|             |\n|             |\n|      4      |\n|             |\n|             |\n|             |\n---------------";
-            return x;
-        }
-        else if(card == 5)
-        {
-            String x = " _____________\n|             |\n|             |\n|             |\n|      5      |\n|             |\n|             |\n|             |\n---------------";
-            return x;
-        }
-        else if(card == 6)
-        {
-            String x = " _____________\n|             |\n|             |\n|             |\n|      6      |\n|             |\n|             |\n|             |\n---------------";
-            return x;
-        }
-        else if(card == 7)
-        {
-            String x = " _____________\n|             |\n|             |\n|             |\n|      7      |\n|             |\n|             |\n|             |\n---------------";
-            return x;
-        }
-        else if(card == 8)
-        {
-            String x = " _____________\n|             |\n|             |\n|             |\n|      8      |\n|             |\n|             |\n|             |\n---------------";
-            return x;
-        }
-        else if(card == 9)
-        {
-            String x = " _____________\n|             |\n|             |\n|             |\n|      9      |\n|             |\n|             |\n|             |\n---------------";
-            return x;
-        }
-        else if(card == 10)
-        {
-            String x = " _____________\n|             |\n|             |\n|             |\n|      10     |\n|             |\n|             |\n|             |\n---------------";
-            return x;
-        }
-        else if(card == 11)
-        {
-            String x = " _____________\n|             |\n|             |\n|             |\n|      J      |\n|             |\n|             |\n|             |\n---------------";
-            return x;
-        }
-        else if(card == 12)
-        {
-            String x = " _____________\n|             |\n|             |\n|             |\n|      Q      |\n|             |\n|             |\n|             |\n---------------";
-            return x;
-        }
-        else if(card == 13)
-        {
-            String x = " _____________\n|             |\n|             |\n|             |\n|      K      |\n|             |\n|             |\n|             |\n---------------";
-            return x;
-        }
-        else
-        {
-            String e = "error";
-            return e;
-        }
-
-      
-    }
-    
-  }
 }
