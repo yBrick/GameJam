44485bef6da36fda7108b62e5ba777be24767fde
yBrick <74314017+yBrick@users.noreply.github.com>
2021-05-18 10:28:20 -0500

    Update Dealer.java

--- Dealer.java
+++ Dealer.java
@@ -2,22 +2,51 @@ import java.util.Random;
 import java.util.Arraylist;
 public class Dealer
 {
+  GameJam g = new GameJam();
   Blackjack b = new Blackjack();
   Random r = new Random();
   int cardAmt = 2;
-  public void Hit()
+  public void DealerHit()
   {
    
   }
   
-  public void Stay()
+  public void DealerStay()
   {
     
   }
   
+  public void PlayerHit()
+  {
+  }
+  
+  public void PlayerHit()
+  {
+  }
+  
   public void Deal()
   {
-    
+    int Dealer1 = cardGen();
+    int Dealer2 = cardGen();    
+    b.DealerCards.add(Dealer1);
+    b.DealerCards.add(Dealer2);
+    System.out.println("Dealer's Cards:");
+    c.printCards(b.DealerCards)
+      if(Dealer1 + Dealer2 >= 21)
+    {
+      g.endGame();
+    }
+    int Player1 = cardGen();
+    int Player2 = cardGen();
+   
+    b.PlayerCards.add(Player1);
+    b.PlayerCards.add(Player2);
+    System.out.println("\nPlayer's Cards:");
+    c.printCards(b.PlayerCards)
+       if(Player1 + Player2 >= 21)
+    {
+      g.endGame();
+    }
   }
   public int cardGen()
   {
@@ -28,7 +57,7 @@ public class Dealer
     for(i : b.DealerCards)
     {
        int DealerAmount = 0;
-      DealerAmount = DealerAmount + DealerCards.get(i);
+      DealerAmount = DealerAmount + b.DealerCards.get(i);
       if(DealerAmount > 17)
       {
         boolean DealerStay = true;
