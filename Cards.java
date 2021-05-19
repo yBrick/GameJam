2eb34c649e16a4bb37a9f088a244852eafc79ef1
Joe-Stanek <84087946+Joe-Stanek@users.noreply.github.com>
2021-05-18 22:41:58 -0500

    Update Cards.java

--- Cards.java
+++ Cards.java
@@ -39,5 +39,12 @@ public class Cards
             }
         }
     }
+    
+    public void Rules()
+    {
+        System.out.println("The rules are simple, the goal of the game is to be the closest one to 21. \n Both you and the dealer start with two cards, you can see yours \n but you can only see one of the dealers cards. \n you have a choice to either hit or stay \n hitting gives you another card \n while staying allows you to not take one. \n if somebody goes over 21, they lose!, and if you hit exactly 21, you win!");
+    
+        System.out.println("Helpful Tip: The dealer has a point where they will no longer hit if they are at or above a certain value of cards. \n In this game that number is 17! once the dealer goes above that or hits it, they will no longer draw cards.");
+    }
 
 }
