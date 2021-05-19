2a420cc6b96460706439ca86ad67b02243a811d3
Joe-Stanek <84087946+Joe-Stanek@users.noreply.github.com>
2021-05-18 22:35:15 -0500

    Update GameJam.java

--- GameJam.java
+++ GameJam.java
@@ -3,8 +3,8 @@ public class GameJam
 {
   Dealer d = new Dealer();
   
-  int dealerAmount = d.DealerAmount();
-  int playerAmount = d.PlayerAmount();
+  int dealerAmount = d.DealerAmount;
+  int playerAmount = d.PlayerAmount;
   
   public static void main (String [] args)
   {
