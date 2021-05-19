e9b54d1d4c2d0b4cff57fa299729472ce8542dde
yBrick <74314017+yBrick@users.noreply.github.com>
2021-05-19 00:27:47 -0500

    Update GameJam.java

--- GameJam.java
+++ GameJam.java
@@ -1,10 +1,8 @@
 import java.util.Scanner;
 public class GameJam
 {
-  Dealer d = new Dealer();
-  
-  int dealerAmount = d.DealerAmount;
-  int playerAmount = d.PlayerAmount;
+ 
+
   
   public static void main (String [] args)
   {
@@ -31,41 +29,7 @@ public class GameJam
         while1 = true;
         System.out.println("Please type a valid option.");
       }
-      
-      
-      public void endGame //im fairly sure we still have to create the playerAmount int, but other than that i think this works
-      {
-       if(dealerAmount = 21)
-       {
-         System.out.println("Dealer has 21, Dealer wins!");
-       }
-        else if(playerAmount = 21)
-        {
-          System.out.println("You have 21, blackjack!");
-        }
-         else if(dealerAmount > 21)
-        {
-          System.out.println("Dealer has " + dealerAmount + ". Dealer has gone bust, player wins!");
-        }
-         else if(playerAmount > 21)
-        {
-          System.out.println("Dealer has " + dealerAmount + ". Player has " + playerAmount + ". Player has bust, Dealer wins!");
-        }
-         else if(dealerAmount && playerAmount < 21)
-        {
-           if(dealerAmount > playerAmount)
-           {
-             System.out.println("Dealer has " + dealerAmount + ". Player has " + playerAmount + ". Dealer wins!")
-           }
-           else if(playerAmount > dealerAmount)
-           {
-             System.out.println("Dealer has " + dealerAmount + ". Player has " + playerAmount + ". Player wins!")
-           }
-           else
-           {
-             System.out.println("Dealer has " + dealerAmount + ". Player has " + playerAmount + ". Its a a tie! Dealer wins!")
-           }
-        }
+     
          
         
       }
